756298acc637b737df83fa9dd9dee2e83e205a8b
Yi <yizhang2@gmail.com>
2018-02-10 00:16:40 -0500

    Code working as checked by the example input and output

---
+++ CountingCharacters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+
+
+            foreach (char c in s)
+            {
+                if (!counts.ContainsKey(c))
+                {
+                    counts.Add(c, 1);
+                }
+                else
+                {
+                    counts[c] += 1;
+
+                }
+            }
+            foreach (KeyValuePair<char, int> count in counts)
+            {
+                Console.WriteLine(count.Key + ": " +  count.Value );
+            }
+
+
+
+        }
+    }
+}
