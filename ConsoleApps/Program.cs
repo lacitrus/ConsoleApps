a6f515b1eb20a066febe2a18accf6fca9a884507
Yi <yizhang2@gmail.com>
2018-02-09 22:43:54 -0500

    Add project files.

---
+++ ConsoleApps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
